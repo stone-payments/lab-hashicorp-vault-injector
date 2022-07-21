18e08917897097f0f1e088fa65fb018cd98936df
GersonAS <gersonaquino.sa@gmail.com>
2022-07-21 19:23:01 -0300

    create second app auth

--- policy.tf
+++ policy.tf
@@ -11,3 +11,16 @@ path "database/rds/postgres/vault-hashi-talks-mock/*" {
 }
 EOT
 }
+
+resource "vault_policy" "appruan" {
+  name = "appruan"
+
+  policy = <<EOT
+path "internal/data/database/config" {
+  capabilities = ["read"]
+}
+path "database/rds/postgres/vault-hashi-talks-mock/*" {
+  capabilities = ["read"]
+}
+EOT
+}
