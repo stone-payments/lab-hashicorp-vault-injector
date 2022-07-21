1791b47e49b4bdd81f2fb2af0051caf57923056a
GersonAS <gersonaquino.sa@gmail.com>
2022-07-21 18:52:44 -0300

    create database engine

--- var.tf
+++ var.tf
@@ -1,3 +1,9 @@
 locals {
   VAULT_ADDR = "http://vault-talks.homolog.stone.credit:8200"
+  username_db = "***REMOVED***_username"
+  password_db = ""
+  database_name = "***REMOVED***"
+  endpoint  = "vault-hashi-talks-mock-7e54ac90.cexop9zvz2f9.us-east-1.rds.amazonaws.com"
+  database_port = "5432"
+
  }
