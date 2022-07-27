588db881436d83c0343d8a89f53482545c6048d3
Igor Luiz de Sousa Santos <73206099+igoritos22@users.noreply.github.com>
2022-07-27 14:16:04 -0300

    remove database info (#5)
    * remove database info
    
    * ajustes

--- setup/backend-auth.tf
+++ setup/backend-auth.tf
@@ -1,7 +1,27 @@
+# backend auth aws
+resource "vault_auth_backend" "aws" {
+  type = "aws"
+}
+
+resource "vault_aws_auth_backend_client" "aws" {
+  backend = vault_auth_backend.aws.path
+  access_key = aws_iam_access_key.vault_auth.id
+  secret_key = aws_iam_access_key.vault_auth.secret
+}
+
+resource "vault_aws_auth_backend_role" "aws_auth_role_with_web_identity" {
+  role      = "app-example-iam-auth"
+  auth_type = "iam"
+  bound_iam_principal_arns = [aws_iam_role.iam_assumable_role_with_web_identity.arn]
+  token_period  = 240
+  token_max_ttl = 2160
+  token_policies = ["app-example-iam-auth"]
+}
 resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
+# backend auth kubernetes
 resource "vault_kubernetes_auth_backend_config" "kubernetes" {
   backend                = vault_auth_backend.kubernetes.path
   kubernetes_host        = "local.kubernetes_host"
@@ -18,6 +38,7 @@ resource "vault_kubernetes_auth_backend_role" "kubernetes" {
   token_policies                   = ["${vault_policy.kubernetes.name}"]
 }
 
+# backend auth app role
 resource "vault_auth_backend" "approle" {
   type = "approle"
 
